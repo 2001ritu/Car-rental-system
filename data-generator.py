21725b8e9483b335e0aa36e781d8a12bccd24535
2001ritu <66665883+2001ritu@users.noreply.github.com>
2021-05-26 20:01:51 +0530

    Update data-generator.py

--- data-generator.py
+++ data-generator.py
@@ -5,8 +5,7 @@ data = {
 }
 
 for i in range(0,1000):
-    data['points'].append({"id":i,"x":i+6.765,"y":i+1.354})
-	
+    data['points'].append({"id":i,"x":i*1.765,"y":i*1.354})		
 
 with open('vehicle-database.json', 'w') as outfile:
     json.dump(data, outfile)
