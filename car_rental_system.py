74e112f0d24d96d454cab79debd3bc5bf4493f7a
2001ritu <66665883+2001ritu@users.noreply.github.com>
2021-05-26 19:45:05 +0530

    Create car_rental_system.py

---
+++ car_rental_system.py
@@ -0,0 +1,27 @@
+import math
+import json
+
+
+f = open('vehicle_data.json')
+data=json.load(f)
+f.close
+
+carlocations=[]
+for i in data['points']:
+    carlocations.append(i)
+
+def FetchCabs(a,b):
+    carlocations.sort(key=lambda x: math.sqrt((float(x['x']) - a) ** 2 + (float(x['y']) - b) ** 2))
+
+    output = []
+    for j in range(0,5):
+        output.append(carlocations[j]);
+
+    for i in output:
+        print(i)
+
+x=float(input())
+y=float(input())
+
+
+FetchCabs(x,y)
